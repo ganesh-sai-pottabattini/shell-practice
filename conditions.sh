df9bf90086b35997d9d072d2624616bcc6e799fa
Ganesh <ganesh@gmail.com>
2026-05-24 21:03:55 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -11,12 +11,20 @@
 # If, else, elif, nested if conditions
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-echo " please give the values "
-read num1
-read num2
+# echo " please give the values "
+# read num1
+# read num2
 
-# If two even numbers are passed by the user, 
+# # If two even numbers are passed by the user, 
 
-if [ $((num1 % 2)) -eq 0 ] && [ $((num2 % 2)) -eq 0 ]; then
-echo " addition is : $((num1 + num2)) "
+# if [ $((num1 % 2)) -eq 0 ] && [ $((num2 % 2)) -eq 0 ]; then
+# echo " addition is : $((num1 + num2)) "
+# fi
+
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ]; then
+echo " You are not root user "
 fi
+else 
+echo " I am continuing "
