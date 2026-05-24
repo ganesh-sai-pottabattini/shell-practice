0bca5a6263a4d8d5d20676af836f954d638c9308
Ganesh <ganesh@gmail.com>
2026-05-24 22:01:48 +0530

    conditions

--- functions.sh
+++ functions.sh
@@ -3,24 +3,49 @@
 # Installing mysql and nginx and anyother using functions, coz if we place the repetetive steps in a function, code will be reduced.
 # Just calling the function is enough.
 
-user_id=$(id -u)
+# user_id=$(id -u)
 
-if [ $user_id -ne 0 ]; then
+# if [ $user_id -ne 0 ]; then
 
-   echo " You are not root user "
-   exit 1
+#    echo " You are not root user "
+#    exit 1
+
+#    validate()
+#    {
+#       if [ $2 -eq 0 ]; then
+#          echo " $1 installation is successful "
+#          else
+#          echo " $1 installation is failed "
+#       fi
+#    }
+
+# else
+#    dnf list installed mysql -y
+#    if [ $? -eq 0 ]; then
+#       echo " mysql is already installed "
+#       exit 1
+#       else 
+#       dnf install mysql -y
+#       validate mySQL $?
+#    fi
+# fi
+
+User_Id=$(id -u)
+if [ User_Id -ne 0 ]; then
+echo " need to be run as root user, use sudo "
+exit 1
 
 else
-   dnf list installed mysql -y
-   if [ $? -eq 0 ]; then
-      echo " mysql is already installed "
-      exit 1
-      else 
-      dnf install mysql -y
-      if [ $? -eq 0 ]; then
-         echo " mySQL installation is successful "
-         else
-         echo " mySQL installation is failed "
-      fi
-   fi
+dnf list installed mySQL -y
+
+if [ $? -eq 0 ]; then
+echo " already installed
+else
+dnf install mySQL -y
+if [ $? -eq 0 ]; then
+echo " mySQL installation is successful "
+else 
+echo " mySQL installation is failed "
+fi
+fi
 fi
