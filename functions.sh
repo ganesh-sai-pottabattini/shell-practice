1e5dbb01bf95079427b1ba81ccf2a71dd52d28d3
Ganesh <ganesh@gmail.com>
2026-05-24 22:17:57 +0530

    conditions

--- functions.sh
+++ functions.sh
@@ -30,21 +30,48 @@
 #    fi
 # fi
 
+# User_Id=$(id -u)
+# if [ User_Id -ne 0 ]; then
+#    echo " need to be run as root user, use sudo "
+#    exit 1
+
+#    else
+#       dnf list installed mySQL -y
+#       if [ $? -eq 0 ]; then
+#          echo " already installed "
+#          else
+#             dnf install mySQL -y
+#                if [ $? -eq 0 ]; then
+#                   echo " mySQL installation is successful "
+#                   else 
+#                      echo " mySQL installation is failed "
+#                 fi
+#        fi
+# fi
+
+
 User_Id=$(id -u)
 if [ User_Id -ne 0 ]; then
    echo " need to be run as root user, use sudo "
    exit 1
+	else
 
-   else
-      dnf list installed mySQL -y
-      if [ $? -eq 0 ]; then
-         echo " already installed "
-         else
-            dnf install mySQL -y
-               if [ $? -eq 0 ]; then
-                  echo " mySQL installation is successful "
-                  else 
-                     echo " mySQL installation is failed "
-                fi
-       fi
+	validate() mySQL
+	validate () nginx
+
+
+	validate()
+	{
+		dnf list installed $1 -y
+		if [ $? -eq 0 ]; then
+         		echo " $1 already installed "
+			else
+            		dnf install $1 -y
+               			if [ $? -eq 0 ]; then
+                  			echo " $1 installation is successful "
+                  			else 
+                     			echo " $1 installation is failed "
+	                        fi
+		fi
+	}
 fi
