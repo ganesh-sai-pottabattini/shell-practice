83018a6aab69c42b49cfc92b33a1568a0de43360
Ganesh <ganesh@gmail.com>
2026-05-27 16:11:06 +0530

    roboshop2

--- 2roboshop.sh
+++ 2roboshop.sh
@@ -16,7 +16,7 @@ fi
 ACTION=$1
 shift 
 
-if [ "$ACTION" != "create"] && [ "$ACTION" != "delete" ]; then
+if [ $ACTION != "create"] && [ $ACTION != "delete" ]; then
 echo -e "$R ERROR:: First argument must be either create or delete $N"
 echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
 exit 1
